16ebf75c5b6f88eea7e418f413768fd20f6d7e56
kyeongwxx <kyeongwxx@gmail.com>
2022-12-09 00:42:04 +0900

    feat: navbar 구현
    bootstrap 설치 및 css 오버라이드

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">SimpleShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
