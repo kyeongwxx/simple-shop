24cdcec787db7b2764316c6dfa4fccaf83ab7223
kyeongwxx <kyeongwxx@gmail.com>
2022-12-11 23:03:14 +0900

    feat: Detail 페이지 데이터바인딩
    find 메서드, usePrams() 활용하여 조건에 맞는 데이터 필터링

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,20 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
-export const Detail = () => {
+export const Detail = (props) => {
+  let { id } = useParams();
+  let product = props.data.find((el) => el.id === +id);
+
   return (
     <Container>
       <Row>
         <Col md={6}>
           <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            src={`https://codingapple1.github.io/shop/shoes${
+              product.id + 1
+            }.jpg`}
             width="100%"
             alt=""
           />
         </Col>
         <Col md={6}>
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{product.title}</h4>
+          <p>{product.content}</p>
+          <p>{product.price.toLocaleString()}원</p>
           <button className="btn btn-danger">주문하기</button>
         </Col>
       </Row>
