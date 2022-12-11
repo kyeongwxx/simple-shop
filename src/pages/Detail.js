bfebac99096140b6f79079b78659d0d974be19d2
kyeongwxx <kyeongwxx@gmail.com>
2022-12-12 00:01:20 +0900

    feat: 일정 시간 후 특정 UI가 동적으로 사라지는 효과 구현
    setInterval() 함수 및 useEffect() 활용

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,11 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Button = styled.button`
   background: ${(props) => props.bg};
-  color: ${(props) => props.bg === "#DC3545" ? "white" : "black"};
+  color: ${(props) => (props.bg === "#DC3545" ? "white" : "black")};
   padding: 10px;
   border: none;
   border-radius: 10px;
@@ -13,9 +14,18 @@ const Button = styled.button`
 export const Detail = (props) => {
   let { id } = useParams();
   let product = props.data.find((el) => el.id === +id);
+  
+  let [sale, setSale] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setSale(false);
+    }, 2000);
+  });
 
   return (
     <Container>
+      {sale && <div className="alert alert-warning">2초이내 구매시 할인</div>}
       <Row>
         <Col md={6}>
           <img
