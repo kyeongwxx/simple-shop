fe8875a79bdbb1232208408c3938c740889647ce
kyeongwxx <kyeongwxx@gmail.com>
2022-12-23 01:33:06 +0900

    refactor: TabContent 컴포넌트 코드 리팩터링
    Before: 세 번째 피연산자가 null인 삼항 연산자
    After: 논리 연산자 && 사용

--- src/components/TabContent.js
+++ src/components/TabContent.js
@@ -16,9 +16,9 @@ export const TabContent = (props) => {
 
   return (
     <div className={`opacity-start ${opacity}`}>
-      {props.tabContent === 0 ? <div>내용0</div> : null}
-      {props.tabContent === 1 ? <div>내용1</div> : null}
-      {props.tabContent === 2 ? <div>내용2</div> : null}
+      {props.tabContent === 0 && <div>내용0</div>}
+      {props.tabContent === 1 && <div>내용1</div>}
+      {props.tabContent === 2 && <div>내용2</div>}
     </div>
   );
 };
